36ade3964df34cb8bbd96faab5f69084837c0203
aboubakr el habti <aboubakr.elhabti@gmail.com>
2021-07-24 11:59:50 +0100

    adding (reset password )

--- project/settings.py
+++ project/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-915#!_^*6$uf%q*%i3&ks59u+o^o@*69psb_=%(w5rw%uzo^fd'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -147,3 +147,12 @@ LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
 
 AUTOTRANSLATE_TRANSLATOR_SERVICE = 'autotranslate.services.GoogleAPITranslatorService'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import views
+from django.contrib.auth import views
 from django.urls import path
+
 from .views import *
 
 
@@ -11,4 +13,10 @@ urlpatterns = [
     path('profile/<str:id>/edit/',profile_edit,name="profile_edit"),
     path('password_change/',views.PasswordChangeView.as_view(template_name="password_change_form.html"),name="password_change"),
     path('password_change_done/',views.PasswordChangeDoneView.as_view(template_name="password_change_done.html"),name="password_change_done"),
+
+    path('password-reset/',views.PasswordResetView.as_view(template_name='reset/password_reset_form.html'),name="password_reset"),
+    path('password-reset/done/',views.PasswordResetDoneView.as_view(template_name='reset/password_reset_done.html'),name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>/',views.PasswordResetConfirmView.as_view(template_name='reset/password_reset_confirm.html'),name="password_reset_confirm"),
+    path('password-reset-complete',views.PasswordResetCompleteView.as_view(template_name='reset/password_reset_complete.html'),name="password_reset_complete"),
+
 ]
