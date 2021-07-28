10c54f15441e91682c92438ef95d3371f555b0a9
aboubakr el habti <aboubakr.elhabti@gmail.com>
2021-07-28 14:43:27 +0100

    updated the views

--- post/views.py
+++ post/views.py
@@ -154,7 +154,7 @@ def jobs(request):
     filter = JobFilter(request.GET, queryset=posts)
     posts = filter.qs
 
-    paginator = Paginator(posts, 2)
+    paginator = Paginator(posts, 9)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
