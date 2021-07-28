619b94eacc49bd268e5b418477e429a74bb79b6e
aboubakr el habti <aboubakr.elhabti@gmail.com>
2021-07-28 17:33:01 +0100

    added ssl

--- project/settings.py
+++ project/settings.py
@@ -171,3 +171,8 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
+
+if os.getcwd() == '/app':
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO','https')
+    SECURE_SSL_REDIRECT = True
+    DEBUG = False
