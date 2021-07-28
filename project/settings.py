4fb726ab0bc45eaa95bfb3a9f425b2c2f017798e
aboubakr el habti <aboubakr.elhabti@gmail.com>
2021-07-28 15:32:23 +0100

    added django heroku

--- project/settings.py
+++ project/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import django_heroku
 import os
 
 
@@ -168,3 +169,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+django_heroku.settings(locals())
