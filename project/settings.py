8ef6185896c64db4b3b66c433fae41b31190eb42
aboubakr el habti <aboubakr.elhabti@gmail.com>
2021-07-29 18:45:32 +0100

    updated settings

--- project/settings.py
+++ project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'eazymax.herokuapp.com', 'www.eazymax.com','eazymax.com']
 
