10cfb557d1f28b0acd9696f868db26eb7cb77ca1
aboubakr el habti <aboubakr.elhabti@gmail.com>
2021-08-11 17:02:04 +0100

    updated admin panel

--- post/admin.py
+++ post/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Post , Category
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title","poster","phone_number","category","hot","date_posted"]
+
+admin.site.register(Post,PostAdmin)
 admin.site.register(Category)
--- user/admin.py
+++ user/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user","sta","prem","vip","expiredate","category","phone_number"]
+
+admin.site.register(Profile,ProfileAdmin)
+admin.site.site_header = "EazyMax"
+admin.site.site_title = "EazyMax"
